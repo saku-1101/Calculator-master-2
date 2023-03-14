e9f457341173c278f02056409cf5cfcfc315ae62
saku-1101 <sakupi1101@outlook.jp>
2023-03-14 22:25:18 +1100

    [add]Parse to Integer with exit code error

--- calc/main.swift
+++ calc/main.swift
@@ -12,9 +12,42 @@ var args = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
 
 // Retrieve User Input
-//let no1 = args[0]; // Sample Code Only! Update Required!
-//let operator = args[1]; // Sample Code Only! Update Required!
-//let no2 = args[2]; // Sample Code Only! Update Required!
+let operators = ["+", "-", "x", "/", "%"]
+//let no1 = Int(args[0]); // Sample Code Only! Update Required!
+//let opr = args[1]; // Sample Code Only! Update Required!
+//let no2 = Int(args[2]); // Sample Code Only! Update Required!
+
+// Checking if the command line arguments are valid. If not your program must generate an appropriate error message and then terminate with nonzero exit status.
+
+// Division by zero errors and numeric out-of-bounds errors.
+
+// Parse Integer //
+    // Default values //
+var no1: Int = 0
+var opr: String = ""
+var no2: Int = 0
+if let val = Int(args[0]){
+    no1 = val
+    print(no1)
+}else{
+    print("exit with nonzero status on invalid input: \(args)")
+}
+if operators.contains(args[1]){
+    opr = args[1]
+    print(opr)
+}else{
+    print("exit with nonzero status on invalid input: \(args)")
+}
+if let val = Int(args[2]){
+    no2 = val
+    print(no2)
+}else{
+    print("exit with nonzero status on invalid input: \(args)")
+}
+print(no1, opr, no2)
+
+
+
 
 // Initialize a Calculator object
 let calculator = Calculator();
