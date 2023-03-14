84855e3b2542ebc0ac836af71d26d5fd8e15c325
saku-1101 <sakupi1101@outlook.jp>
2023-03-15 09:58:46 +1100

    [refactor]pass args itself to the calculate function

--- calc/Calculator.swift
+++ calc/Calculator.swift
@@ -12,7 +12,10 @@ class Calculator {
     
     /// For multi-step calculation, it's helpful to persist existing result
     var currentResult = 0;
-    
+    let operators = ["+", "-", "x", "/", "%"]
+    var no1: Int = 0
+    var opr: String = ""
+    var no2: Int = 0
     /// Perform Addition
     ///
     /// - Author: Jacktator
@@ -26,12 +29,60 @@ class Calculator {
     func add(no1: Int, no2: Int) -> Int {
         return no1 + no2;
     }
+    func subtract(no1: Int, no2: Int) -> Int {
+        return no1 - no2;
+    }
+    func multiply(no1: Int, no2: Int) -> Int {
+        return no1 * no2;
+    }
+    func divide(no1: Int, no2: Int) -> Int {
+        // Division by zero errors and numeric out-of-bounds errors.
+        return no1 / no2;
+    }
+    func remaind(no1: Int, no2: Int) -> Int {
+        return no1 % no2;
+    }
     
     func calculate(args: [String]) -> String {
         // Todo: Calculate Result from the arguments. Replace dummyResult with your actual result;
-        let dummyResult = add(no1: 1, no2: 2);
+        if let val = Int(args[0]){
+            self.no1 = val
+            print(self.no1)
+        }else{
+            print("exit with nonzero status on invalid input: \(args)")
+        }
+//        if operators.contains(args[1]){
+//            opr = args[1]
+//            print(opr)
+//        }else{
+//            print("exit with nonzero status on invalid input: \(args)")
+//        }
+        if let val = Int(args[2]){
+            self.no2 = val
+            print(self.no2)
+        }else{
+            print("exit with nonzero status on invalid input: \(args)")
+        }
+        self.opr = args[1]
+        print(no1, opr, no2)
         
-        let result = String(dummyResult);
+        switch opr {
+        case operators[0]:
+            self.currentResult = add(no1: no1, no2: no2);
+        case operators[1]:
+            self.currentResult = subtract(no1: no1, no2: no2);
+        case operators[2]:
+            self.currentResult = multiply(no1: no1, no2: no2);
+        case operators[3]:
+            self.currentResult = divide(no1: no1, no2: no2);
+        case operators[4]:
+            self.currentResult = remaind(no1: no1, no2: no2);
+        default:
+            print("exit with nonzero status on invalid input: \(args)")
+        }
+        
+        
+        let result = String(self.currentResult);
         return(result)
     }
 }
--- calc/main.swift
+++ calc/main.swift
@@ -12,39 +12,36 @@ var args = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
 
 // Retrieve User Input
-let operators = ["+", "-", "x", "/", "%"]
+
 //let no1 = Int(args[0]); // Sample Code Only! Update Required!
 //let opr = args[1]; // Sample Code Only! Update Required!
 //let no2 = Int(args[2]); // Sample Code Only! Update Required!
 
-// Checking if the command line arguments are valid. If not your program must generate an appropriate error message and then terminate with nonzero exit status.
-
-// Division by zero errors and numeric out-of-bounds errors.
-
 // Parse Integer //
     // Default values //
-var no1: Int = 0
-var opr: String = ""
-var no2: Int = 0
-if let val = Int(args[0]){
-    no1 = val
-    print(no1)
-}else{
-    print("exit with nonzero status on invalid input: \(args)")
-}
-if operators.contains(args[1]){
-    opr = args[1]
-    print(opr)
-}else{
-    print("exit with nonzero status on invalid input: \(args)")
-}
-if let val = Int(args[2]){
-    no2 = val
-    print(no2)
-}else{
-    print("exit with nonzero status on invalid input: \(args)")
-}
-print(no1, opr, no2)
+//var no1: Int = 0
+//var opr: String = ""
+//var no2: Int = 0
+// Checking if the command line arguments are valid. If not your program must generate an appropriate error message and then terminate with nonzero exit status.
+//if let val = Int(args[0]){
+//    no1 = val
+//    print(no1)
+//}else{
+//    print("exit with nonzero status on invalid input: \(args)")
+//}
+//if operators.contains(args[1]){
+//    opr = args[1]
+//    print(opr)
+//}else{
+//    print("exit with nonzero status on invalid input: \(args)")
+//}
+//if let val = Int(args[2]){
+//    no2 = val
+//    print(no2)
+//}else{
+//    print("exit with nonzero status on invalid input: \(args)")
+//}
+//print(no1, opr, no2)
 
 
 
@@ -53,6 +50,6 @@ print(no1, opr, no2)
 let calculator = Calculator();
 
 // Calculate the result
-let result = calculator.add(no1: 1, no2: 1);
+let result = calculator.calculate(args: args);
 
 print(result)
