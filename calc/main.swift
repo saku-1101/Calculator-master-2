6e00f006dc334a108c07029104f88d609b7009c0
saku-1101 <sakupi1101@outlook.jp>
2023-03-15 16:05:05 +1100

    [add]parser and error checker is introduced

--- calc/main.swift
+++ calc/main.swift
@@ -5,47 +5,61 @@
 //  Created by Jesse Clark on 12/3/18.
 //  Copyright © 2018 UTS. All rights reserved.
 //
-
 import Foundation
 
+// Retrieve User Input
 var args = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
+let operators: [String] = ["+", "-", "/", "%", "x"]
+var number: Bool = true
+var op: Bool = false
 
-// Retrieve User Input
+// Parser
+func isStringToInt(string: String) -> Bool {
+    return Int(string) != nil
+}
 
-//let no1 = Int(args[0]); // Sample Code Only! Update Required!
-//let opr = args[1]; // Sample Code Only! Update Required!
-//let no2 = Int(args[2]); // Sample Code Only! Update Required!
+//------------- ERROR CHECKER --------------------------------------------------//
+// Test if the arguments can be parsed to Integer
+if args.count == 1 && isStringToInt(string: String(args[0])) == true {
+    let result = Int(args[0]);
+    print(result!);
+    exit(EXIT_FAILURE)
+}
 
-// Parse Integer //
-    // Default values //
-//var no1: Int = 0
-//var opr: String = ""
-//var no2: Int = 0
-// Checking if the command line arguments are valid. If not your program must generate an appropriate error message and then terminate with nonzero exit status.
-//if let val = Int(args[0]){
-//    no1 = val
-//    print(no1)
-//}else{
-//    print("exit with nonzero status on invalid input: \(args)")
-//}
-//if operators.contains(args[1]){
-//    opr = args[1]
-//    print(opr)
-//}else{
-//    print("exit with nonzero status on invalid input: \(args)")
-//}
-//if let val = Int(args[2]){
-//    no2 = val
-//    print(no2)
-//}else{
-//    print("exit with nonzero status on invalid input: \(args)")
-//}
-//print(no1, opr, no2)
+// Test if array has more than 3 elements and return error. Also if the array ends in number.
+if args.count < 3 || args.count%2 == 0{
+    print("Invalid input")
+    exit(EXIT_FAILURE)
+}
 
+// Check arguments are in correct form
+for element in args{
+    if (operators.contains(String(element)) == false && isStringToInt(string: String(element)) == false){ // No operator and integers
+        print("Invalid input format: Input has to contain int and operators only")
+        exit(EXIT_FAILURE)
+    } else {
+        // Test if the format is [number operator number ... number]
+        if (isStringToInt(string: String(element)) && op == false){
+            // Proof of the existence of the number
+            // when number flag is on, operator flag is off
+            number = false;
+            op = true;
+        } else if (operators.contains(String(element)) && number == false){
+            // Proof of the existence of the operator
+            // when number flag is off, operator flag is on
+            number = true;
+            op = false;
+        }
+        else {
+            print("Invalid input format: must be in form [number operator number ... number]")
+            exit(EXIT_FAILURE)
+            // Exit code
+        }
+    }
+}
 
-
-
+//------------------------------------------------------------------------------------//
 // Initialize a Calculator object
 let calculator = Calculator();
 
